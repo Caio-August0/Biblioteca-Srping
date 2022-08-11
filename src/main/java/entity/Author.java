8a290767f42a8908b45904d384a9b503067b3844
Caio Augusto <caioaugustobr0106@gmail.com>
2022-08-11 11:48:22 -0300

    first commit-Developed project entities

---
+++ src/main/java/entity/Author.java
@@ -0,0 +1,22 @@
+package entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer age;
+}
---
+++ src/main/java/entity/Book.java
@@ -0,0 +1,37 @@
+package entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome", nullable = false, unique = true)
+    private String name;
+
+    @Column(name = "paginas", nullable = false)
+    private Integer pages;
+
+    @Column(name = "capitulos", nullable = false)
+    private Integer chapters;
+
+    @Column(nullable = false)
+    private String isbn;
+
+    @Column(name = "nome_do_autor",nullable = false, unique = true)
+    private String publisherName;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
+    @JoinColumn(name = "author_id")
+    private Author author;
+
+}
